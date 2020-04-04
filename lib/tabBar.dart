1173ee55d1cafba9f85104fb706cd458ace5c9e1
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-04 14:15:33 -0300

    create tabsRouter

---
+++ lib/tabBar.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/home.dart';
+import 'package:instagram_clone/profile_screen.dart';
+
+class Tabbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        bottomNavigationBar: TabBar(
+          tabs: [
+            Tab(
+              child: Container(
+                child: Icon(
+                  Icons.home,
+                  size: 25,
+                ),
+              ),
+            ),
+            Tab(
+              child: Container(
+                child: Icon(
+                  Icons.search,
+                  size: 25,
+                ),
+              ),
+            ),
+            Tab(
+              child: Container(
+                child: Icon(
+                  Icons.add_box,
+                  size: 25,
+                ),
+              ),
+            ),
+            Tab(
+              child: Container(
+                child: Icon(
+                  Icons.favorite,
+                  size: 25,
+                ),
+              ),
+            ),
+            Tab(
+              child: Container(
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey[800],
+                  radius: 12,
+                  child:
+                      CircleAvatar(backgroundColor: Colors.green, radius: 10),
+                ),
+              ),
+            ),
+          ],
+          indicatorWeight: 2,
+          labelStyle: TextStyle(fontSize: 10),
+          unselectedLabelColor: Colors.grey[800],
+          labelColor: Colors.greenAccent[700],
+          indicatorColor: Colors.transparent,
+        ),
+        body: TabBarView(children: [
+          HomePost(),
+          ProfileScreen(),
+          ProfileScreen(),
+          ProfileScreen(),
+          ProfileScreen(),
+        ]),
+      ),
+    );
+  }
+}
